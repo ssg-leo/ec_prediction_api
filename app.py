98dd2d279d1f1c65ba003266ae597f96bb6c4782
Leo Li <leo_li@ssg.gov.sg>
2023-04-05 00:05:37 +0800

    Updated the API to make prediction

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-# app.py
-from fastapi import FastAPI, Depends, HTTPException
+import joblib
+from fastapi import FastAPI
+import pandas as pd
 from pydantic import BaseModel
 from typing import Optional
-import numpy as np
-
-# import my_model_package
 
 app = FastAPI()
+
+# Load the optimized pipeline and explainer when the application starts
+pipeline = joblib.load("production/best_pipeline.pkl")
+explainer = joblib.load("production/explainer.pkl")
 
 
 class InputData(BaseModel):
@@ -26,7 +28,40 @@ def validate_input(input_data: InputData):
     return input_data
 
 
-# @app.post("/predict")
-# def predict(input_data: InputData = Depends(validate_input)):
-#     prediction = my_model_package.predict(np.array([input_data.dict().values()]))
-#     return {"prediction": prediction[0]}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.post("/predict/")
+async def predict(input_data: InputData):
+    # Convert the input data to a DataFrame
+    instance = pd.DataFrame([dict(input_data)])
+    instance.columns = ['x', 'y', 'area', 'floor_range', 'type_of_sale', 'district',
+                        'district_name', 'remaining_lease', 'price_index']
+
+    data_transformed = pipeline.named_steps["mean_encoder"].transform(instance)
+    instance_transformed = pipeline.named_steps["scaler"].transform(data_transformed)
+    instance_transformed = pd.DataFrame(instance_transformed)
+    instance_transformed.columns = ['x', 'y', 'area', 'floor_range', 'type_of_sale', 'district',
+                                    'district_name', 'remaining_lease', 'price_index']
+
+    # Perform a prediction using the pipeline
+    prediction = pipeline.predict(instance_transformed)
+
+    # Compute SHAP values using the explainer
+    shap_values = explainer(instance_transformed)
+    feature_names = instance_transformed.columns
+    label_name = "prediction"
+    shap_values_dict = {}
+    label_name = "prediction"
+    shap_values_dict = {}
+    label_name = "prediction"
+    shap_values_for_label = shap_values[0].values
+    shap_dict = {feature_names[i]: float(shap_values_for_label[i]) for i in range(len(feature_names))}
+    shap_values_dict[label_name] = shap_dict
+
+    # Convert the prediction and SHAP values to a nested dictionary
+    response_content = {"prediction": float(prediction[0]), "shap_values": shap_values_dict}
+
+    return response_content
